949eee081d3c811f073b79f8f27ea37302b6fdb6
Jhoan Gordillo Franco <jhoangf94@gmail.com>
2021-07-02 17:12:44 -0500

    Initial Commit

---
+++ themoviedbapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  themoviedbapp
+//
+//  Created by Jhoan Gordillo Franco on 2/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
